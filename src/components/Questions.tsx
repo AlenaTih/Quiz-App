a3e08be524ca565fdaa30169c426e66ba80d03d7
Alena <alyonatihomirova9@gmail.com>
2024-08-29 19:16:53 +0300

    Update to fetch quiz questions from the API

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -3,9 +3,35 @@ import { useState, useEffect } from "react"
 function Questions() {
     const [questions, setQuestions] = useState([])
 
+    useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5&category=17&type=multiple")
+            .then(response => response.json())
+            .then(data => setQuestions(data.results))
+
+        // console.log(questions)
+
+        return () => {
+            setQuestions([])
+        }
+
+    }, [])
+
+    const questionElements = questions.map((question: any) => {
+        return (
+            <div className="question" key={question.question}>
+                <h4>{question.question}</h4>
+                <p>{question.correct_answer}</p>
+                <p>{question.incorrect_answers.join(", ")}</p>
+            </div>
+        )
+    })
+
     return (
         <div className="questions">
             <h1>Questions</h1>
+            <div className="questions-container">
+                {questionElements}
+            </div>
             <button className="check-button">Check answers</button>
         </div>
     )
