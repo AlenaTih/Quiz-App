cc69656f6d893251ab3dd4be240591d972fa8626
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-08-30 02:10:40 +0300

    Update Question file to make an array of all answers and use radio inputs

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,15 +1,19 @@
 function Question(props: any) {
 
-    const incorrectAnswers = props.incorrectAnswers.map((answer: any) => (
-        <p key={answer}>{answer}</p>
+    const answersArray = [...props.incorrectAnswers, props.correctAnswer]
+
+    const answerElements = answersArray.map((answer: any) => (
+        <label key={answer}>
+            {answer}
+            <input className="answer-option" type="radio" value={answer} name="answer"/>
+        </label>
     ))
 
     return (
         <div className="question">
             <h4>{props.question}</h4>
             <div className="answers">
-                    <p>{props.correctAnswer}</p>
-                    {incorrectAnswers}
+                {answerElements}
             </div>
         </div>
     )
