7f79e8c7a581282c979bab90cc34767fa5845322
Alena <alyonatihomirova9@gmail.com>
2024-08-29 18:07:14 +0300

    Adding Start and Questions pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Start from "./components/Start.tsx"
+import Questions from "./components/Questions.tsx"
 import "./App.css"
 
 function App() {
   return (
     <div className="app-container">
       <h1>Quiz App</h1>
+      <Start />
+      <Questions />
     </div>
   )
 }
