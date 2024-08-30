3904bab911f23878f7dc6d5bb77c2670542c0fa0
Alena <alyonatihomirova9@gmail.com>
2024-08-30 18:55:04 +0300

    Update Question to identify a correct answer

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,11 +1,43 @@
+// import { useState } from "react"
+
 function Question(props: any) {
+    // const [formData, setFormData] = useState({
+    //     answer: ""
+    // })
+
+    // const id = React.useId()
+
+    // function handleChange(event: any) {
+    //     setFormData(prevFormData => {
+    //         return {
+    //             ...prevFormData,
+    //             [event.target.name]: event.target.value
+    //         }
+    //     })
+    // }
+
+    function handleChange(event: any) {
+        console.log(event.target.value)
+
+        if (event.target.value === props.correctAnswer) {
+            console.log("Yay!")
+        }
+    }
 
     const answersArray = [...props.incorrectAnswers, props.correctAnswer]
 
     const answerElements = answersArray.map((answer: any) => (
         <label key={answer}>
             {answer}
-            <input className="answer-option" type="radio" value={answer} name="answer"/>
+            <input
+                className="answer-option"
+                onChange={handleChange}
+                // id={`${id}-answer`}
+                type="radio"
+                value={answer}
+                // checked={formData.answer === answer}
+                // checked={answer}
+                name="answer" />
         </label>
     ))
 
