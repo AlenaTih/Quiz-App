4f0078226d11e79edcce8e4a5bb6d349c9505bb3
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-09-03 02:39:58 +0300

    Update Start

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -3,7 +3,7 @@ function Start(props: any) {
     return (
         <div className="start">
             <h1 className="start-title">Quizzical</h1>
-            <p className="start-text">Are you ready to challenge yourself?</p>
+            <p className="start-text">Are you ready for a challenge?</p>
             <button
                 className="start-button"
                 onClick={props.startQuiz}>
